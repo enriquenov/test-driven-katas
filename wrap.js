9cc6bb0431e93e5aac3b0b7bad73804a761396dc
Neil Scheuermann <neilscheuermann@gmail.com>
2018-11-19 09:11:05 -0700

    working on wrap

--- wrap.js
+++ wrap.js
@@ -1,6 +1,7 @@
 function wrap(line, maxLen) {
   if (!line.length) return line;
-
+  //Should perform some logic here.
+  return line.split(' ').join('\n');
 }
 
 module.exports = wrap;
--- wrap.spec.js
+++ wrap.spec.js
@@ -18,4 +18,9 @@ describe('wrap', () => {
     expect(wrap("", 10)).to.equal("");
   });
 
+  it('first instance of \n should be <= maxLen', () => {
+    line = 'Lorem ipsum dolor sit eu amet, elit na magna sem amet nulla vel purus ac ligula.';
+    maxLen = 20;
+    expect(wrap(line, maxLen).indexOf("\n")).to.lessThan(maxLen).and.greaterThan(-1);
+  })
 });
